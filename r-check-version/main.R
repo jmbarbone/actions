7790e411c5218b7346e79407d8be46b15528d4c1
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-07-23 16:28:19 -0400

    cleanup

--- r-check-version/main.R
+++ r-check-version/main.R
@@ -8,7 +8,7 @@ catln <- function(...) cat(..., "\n", sep = "")
 ca <- command_args()
 ca$add_argument("--directory", default = ".")
 ca$add_argument("--ignore-dev-version", default = FALSE)
-ca$add_argument("--repository")
+ca$add_argument("--repository", default = "jmbarbone/actions")
 args <- ca$parse()
 
 print(args)
